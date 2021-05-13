59939d794b2adedf9a1a61705275b13364089368
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-05-13 14:53:08 -0400

    poketimes

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return (
+        <div className= "container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo possimus omnis ducimus repudiandae quibusdam non optio aut impedit! Quam aliquid numquam architecto voluptas repudiandae! Odio velit maiores voluptatum ullam ducimus?</p>
+        </div>
+    )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/About')
+    // }, 2000);
+    return (
+        <div className= "container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo possimus omnis ducimus repudiandae quibusdam non optio aut impedit! Quam aliquid numquam architecto voluptas repudiandae! Odio velit maiores voluptatum ullam ducimus?</p>
+        </div>
+    )
+}
+
+export default Contact
